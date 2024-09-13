158489054d8300a53a0878d0302ed62f97a3a7a7
Suman Chakrabortty <suman.chakrabortty8514@gmail.com>
2024-09-14 02:19:44 +0530

    option-removed

--- public/Projects/Store/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect("sql12.freesqldatabase.com","sql12731285","uEB39NwBd3","sql12731285") or die(mysqli_error($con));
-//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
-?>
